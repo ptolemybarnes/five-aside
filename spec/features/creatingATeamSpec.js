2befa224289226bc0c71730ab92daf47f70d7390
Ptolemy <ptolemybarnes@gmail.com>
2015-05-27 19:57:38 +0100

    Refactored test with adding player helper methods

--- spec/features/creatingATeamSpec.js
+++ spec/features/creatingATeamSpec.js
@@ -2,25 +2,31 @@ describe('A Captain', function() {
 
   it('knows when a team is valid', function() {
     var team = new Team();
-    for(var i = 0; i < 7; i ++) {
-      team.addPlayer(new MalePlayer());
-    }
-    for(var i = 0; i < 3; i ++) {
-      team.addPlayer(new FemalePlayer());
-    }
+    
+    addMalePlayers(team, 7);
+    addFemalePlayers(team, 3);
 
     expect(team.isValid()).toBe(true);
   });
 
   it('knows when a team is too large', function() {
     var team = new Team();
-    for(var i = 0; i < 10; i ++) {
-      team.addPlayer(new MalePlayer());
-    }
-    for(var i = 0; i < 3; i ++) {
-      team.addPlayer(new FemalePlayer());
-    }
+
+    addMalePlayers(team, 10);
+    addFemalePlayers(team, 3);
 
     expect(team.isValid()).toBe(false);
   });
+
+  function addMalePlayers(team, quantity) {
+    for(var i = 0; i < quantity; i ++) {
+      team.addPlayer(new MalePlayer());
+    }
+  }
+
+  function addFemalePlayers(team, quantity) {
+    for(var i = 0; i < 3; i ++) {
+      team.addPlayer(new FemalePlayer());
+    }
+  }
 });
